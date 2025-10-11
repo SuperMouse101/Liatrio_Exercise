bf9b57d3c63f03a72d6185643fa795f371b568cf
SuperMouse <supermystery.mouse@proton.me>
2025-10-11 13:02:27 -0700

    Now prints current time

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
     "log"
-
+    "time"
     "github.com/gofiber/fiber/v3"
 )
 
@@ -13,7 +13,9 @@ func main() {
     // Define a route for the GET method on the root path '/'
     app.Get("/", func(c fiber.Ctx) error {
         // Send a string response to the client
-        return c.SendString("Hello, World 👋!")
+        r := time.Now().UTC().GoString();
+        r += "\nHello, World 👋!";
+        return c.SendString(r)
     })
 
     // Start the server on port 3000
